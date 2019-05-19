9813391b676406d275663c5ff3b2ba7192fdeea8
Netto Cavalcante <frcnetto@gmail.com>
2019-05-19 12:56:45 -0300

    App MVP done
    # What's new?
    
    + App interface;
    + Dice drawable;
    + Dice roll logic;
    
    # TODO
    
    + Dice animations;
    + Move click to dice and remove button;

--- app/src/main/java/br/com/frcnetto/diceroller/MainActivity.kt
+++ app/src/main/java/br/com/frcnetto/diceroller/MainActivity.kt
@@ -2,11 +2,33 @@ package br.com.frcnetto.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnRoll.setOnClickListener {
+            rollDice()
+        }
+
+    }
+
+    private fun rollDice() {
+        when((Random().nextInt(6) + 1)){
+            1    -> dice.setImageResource(R.drawable.dice_1)
+            2    -> dice.setImageResource(R.drawable.dice_2)
+            3    -> dice.setImageResource(R.drawable.dice_3)
+            4    -> dice.setImageResource(R.drawable.dice_4)
+            5    -> dice.setImageResource(R.drawable.dice_5)
+            6    -> dice.setImageResource(R.drawable.dice_6)
+            else -> dice.setImageResource(R.drawable.empty_dice)
+        }
     }
 }
